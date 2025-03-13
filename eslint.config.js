142e32f68321b885023675796154f65a9e913d45
lgcholoquinga <luisg.choloquinga@gamil.com>
2025-03-13 17:24:38 -0500

    Project init

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+
+module.exports = tseslint.config(
+	{
+		files: ['**/*.ts'],
+		extends: [
+			eslint.configs.recommended,
+			...tseslint.configs.recommended,
+			...tseslint.configs.stylistic,
+			...angular.configs.tsRecommended,
+		],
+		processor: angular.processInlineTemplates,
+		rules: {
+			'no-empty-function': 'off',
+			'@typescript-eslint/no-empty-function': 'off',
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{
+					args: 'all',
+					argsIgnorePattern: '^_',
+					caughtErrors: 'all',
+					caughtErrorsIgnorePattern: '^_',
+					destructuredArrayIgnorePattern: '^_',
+					varsIgnorePattern: '^_',
+					ignoreRestSiblings: true,
+				},
+			],
+		},
+	},
+	{
+		files: ['**/*.html'],
+		extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
+		rules: {},
+	},
+);
